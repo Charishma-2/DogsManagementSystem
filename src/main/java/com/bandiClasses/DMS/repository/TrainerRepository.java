d6119f7fbd435dce2bebf63357c29bbaae6801d6
Charishma-2 <charishmachowdarybandaru@gmail.com>
2023-11-12 22:56:05 -0600

    added files

---
+++ src/main/java/com/bandiClasses/DMS/repository/TrainerRepository.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.bandiClasses.DMS.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.bandiClasses.DMS.Models.Trainer;
+
+/**
+ * @author Charishma Chowdary Bandaru
+ * Nov 12, 2023
+ */
+public interface TrainerRepository extends CrudRepository<Trainer, Integer> {
+
+}
